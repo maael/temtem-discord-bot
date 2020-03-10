c0a77031d486404de130695feb8a3a087e21076f
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-10 11:13:53 +0000

    fix: 🐛 Lint/build issues

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,8 @@ import { Command } from "../types";
 
 export const name = "ping";
 
-const ping: Command = async function(args, msg) {
+const ping: Command = async function(_args, msg) {
+  if (!msg.channel) return;
   await msg.channel.send("pong");
 };
 
--- src/commands/ping2.ts
+++ src/commands/ping2.ts
@@ -2,7 +2,8 @@ import { Command } from "../types";
 
 export const name = "ping2";
 
-const ping2: Command = async function(args, msg) {
+const ping2: Command = async function(_args, msg) {
+  if (!msg.channel) return;
   await msg.channel.send("pong2");
 };
 
